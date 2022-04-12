1046c22fc736caee01c5bc864e7bb570f7b3b4a1
C0159412020 <adlightakinyi1@gmail.com>
2022-04-12 17:24:27 +0300

    edited

--- src/app/quote-info/quote-info.component.ts
+++ src/app/quote-info/quote-info.component.ts
@@ -1,25 +1,10 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-quote-info',
-//   templateUrl: './quote-info.component.html',
-//   styleUrls: ['./quote-info.component.css']
-// })
-// export class QuoteInfoComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
 import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+
 import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-info',
-      // moduleId: module.id,
-     templateUrl: './quote-info.component.html',
+  templateUrl: './quote-info.component.html',
   styleUrls: ['./quote-info.component.css']
 })
 export class QuoteInfoComponent implements OnInit {
@@ -27,29 +12,23 @@ export class QuoteInfoComponent implements OnInit {
 @Input() quote!: Quote;
 @Output() toDelete = new EventEmitter<boolean>();
 
- deleteQuote(remove:boolean){
+deleteQuote(remove:boolean){
   this.toDelete.emit(remove);
-
- }
- numberOfLikes : number = 0;
- numberOfDislikes: number = 0;
- likeButtonClick() {
-  this.numberOfLikes++;
- }
-dislikeButtonClick(){
-   this.numberOfDislikes++;
-
-
 }
-
+numberOfLikes : number = 0;
+numberOfDislikes: number = 0;
+likeButtonClick() {
+  this.numberOfLikes++;
+}
+dislikeButtonClick(){
+  this.numberOfDislikes++;
+}
 
 
 
   constructor() { }
 
   ngOnInit() {
-  
   }
 
 }
-
